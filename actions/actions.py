5cacdd81736e193a0d607d14bb08fccf3dd42bfa
goehringng <goehringng@vcu.edu>
2023-02-20 21:12:15 -0500

    added return data and adjusted fallbacks

--- actions/actions.py
+++ actions/actions.py
@@ -6,30 +6,57 @@
 
 
 
+import json
+from tkinter import EventType
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
-class ActionExploreTrue(Action):
-   def name(self) -> Text:
-      return "action_explore_t"
+class ActionGreetUser(Action):
+    def name(self) -> Text:
+        return "action_display_info"
 
-   def run(self,
-           dispatcher: CollectingDispatcher,
-           tracker: Tracker,
-           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+        s = []
+        for item in tracker.latest_message["entities"]:
+            n = {
+                "entity" : item["entity"],
+                "value" : item["value"]
+            }
+            s.append(n)
+        date_picker = {
+            "intent" : tracker.latest_message["intent"]["name"],
+            "slots" : s
+        }
+        dispatcher.utter_message(text = json.dumps(date_picker, sort_keys=True, indent=2, separators=(',', ': ')))
 
-      return [SlotSet("explore", True)]
+        return []
 
-class ActionExploreFalse(Action):
-   def name(self) -> Text:
-      return "action_explore_f"
+# class ActionExploreTrue(Action):
+#    def name(self) -> Text:
+#       return "action_explore_t"
 
-   def run(self,
-           dispatcher: CollectingDispatcher,
-           tracker: Tracker,
-           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#    def run(self,
+#            dispatcher: CollectingDispatcher,
+#            tracker: Tracker,
+#            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-      return [SlotSet("explore", None)]
+#       return [SlotSet("explore", True)]
+
+# class ActionExploreFalse(Action):
+#    def name(self) -> Text:
+#       return "action_explore_f"
+
+#    def run(self,
+#            dispatcher: CollectingDispatcher,
+#            tracker: Tracker,
+#            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#       return [SlotSet("explore", None)]
