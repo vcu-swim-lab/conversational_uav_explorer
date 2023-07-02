491220c6061c80e16fa1f072074782ef85400d44
Sydney Nguyen <melosydrose@gmail.com>
2023-07-02 17:26:48 -0500

    Add way to record audio and transcription UI

--- gradio_website.py
+++ gradio_website.py
@@ -1,13 +1,25 @@
-from transformers import pipeline
+import openai
 import gradio as gr
 
-p = pipeline("automatic-speech-recognition")
+openai.api_key = "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"
 
 def transcribe(audio):
-    text = p(audio)["text"]
-    return text
+    audio_file = open(audio, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript)
 
-gr.Interface(
-    fn=transcribe, 
-    inputs=gr.Audio(source="microphone", type="filepath"), 
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are part of a UAV and will be given commands to execute."}
+        ]
+    )
+    return transcript["text"]
+
+
+ui = gr.Interface(
+    fn=transcribe,
+    inputs=gr.Audio(source="microphone", type="filepath"),
     outputs="text").launch(share=True)
+
+ui.launch()
