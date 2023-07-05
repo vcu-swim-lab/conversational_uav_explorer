0a5f7816d4889393c670865276be0c70c5b458c4
Sydney Nguyen <melosydrose@gmail.com>
2023-07-05 00:18:25 -0500

    Fix formatting for UAV prompt

--- gradio_website.py
+++ gradio_website.py
@@ -2,13 +2,14 @@ import openai
 import gradio as gr
 
 openai.api_key = "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"
+
 messages = [
-    {"role": "system", "content": "You are an AI-powered chatbot integrated into a UAV"
-                                  " (Unmanned Aerial Vehicle) system. Your purpose is to receive and execute"
+    {"role": "system", "content": "You are an AI-powered chatbot integrated into a UAV "
+                                  "(Unmanned Aerial Vehicle) system. Your purpose is to receive and execute"
                                   "commands from an officer. Your role is to understand and carry out these commands "
-                                  "efficiently. You must acknowledge the command if you understand it. You can respond"
-                                  " to the officer's instructions, ask for clarification if needed, and provide updates"
-                                  " on the execution status of the given commands."}
+                                  "efficiently. You must acknowledge the command if you understand it. Use 2-3 "
+                                  "sentences to respond to the officer's instructions, ask for clarification if "
+                                  "needed, and provide updates on the execution status of the given commands."}
 ]
 
 
@@ -42,15 +43,15 @@ with gr.Blocks() as demo:
     # Conversational UAV Explorer
     Speak into the microphone to give a command.\n\n
     Commands: Explore, Take Picture, Monitor, Stop, Land, Come Back, Left, Right, Up, Down, Get Closer""")
-    with gr.Row().style(equal_height=True):
-        audio_input = gr.Audio(source="microphone", type="filepath")
+    with gr.Row().style():
+        audio_input = gr.Audio(source="microphone", type="filepath", streaming=True)
         output = gr.Textbox(label="Transcript")
     with gr.Row():
         with gr.Column():
             submit_btn = gr.Button("Give command")
             submit_btn.click(fn=transcribe, inputs=audio_input, outputs=output, api_name="record")
-    with gr.Accordion("Example commands:"):
-        gr.Markdown("Explore the floor of the green house.\n\n"
+    with gr.Accordion("Examples:"):
+        gr.Markdown("Explore the second floor of the green house.\n\n"
                     "Take a picture inside the brick house on Main St.\n\n"
                     "Monitor the house at the NE corner of the intersection of Main and Cary.")
 
