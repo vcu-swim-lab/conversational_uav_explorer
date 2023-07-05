da4ddd2bf7b5178b89ded07101c19459e7ba5476
Sydney Nguyen <melosydrose@gmail.com>
2023-07-05 04:22:03 -0500

    Rename to app.py and add tentative approach to integrating LangChain

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import openai
 import gradio as gr
 
@@ -21,6 +22,16 @@ def transcribe(audio):
 
     messages.append({"role": "user", "content": transcript["text"]})
 
+    """Goal: Integrate LangChain into Gradio - my approach is to grab the original transcript above,
+    run it through the chains from chatgpt4uavs.py and save it to filtered_transcript, and use the
+    filtered transcript in the conversation with the conversational UAV.
+    
+    Currently getting a few errors with the sentence_command_chain, specifically the issue lies
+    within PromptTemplate in chatgpt4uavs.py, so I will need to check that. I commented the code
+    out to test the interface without errors."""
+    # filtered_transcript = sentence_command_chain.run({"text": transcript["text"]})
+    # messages.append({"role": "user", "content": filtered_transcript["output"]})
+
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
@@ -38,17 +49,17 @@ def transcribe(audio):
     return chat_transcript
 
 
-with gr.Blocks() as demo:
+with gr.Blocks(theme='sudeepshouche/minimalist') as demo:
     gr.Markdown("""
     # Conversational UAV Explorer
     Speak into the microphone to give a command.\n\n
     Commands: Explore, Take Picture, Monitor, Stop, Land, Come Back, Left, Right, Up, Down, Get Closer""")
     with gr.Row().style():
-        audio_input = gr.Audio(source="microphone", type="filepath", streaming=True)
+        audio_input = gr.Audio(source="microphone", type="filepath")
         output = gr.Textbox(label="Transcript")
     with gr.Row():
         with gr.Column():
-            submit_btn = gr.Button("Give command")
+            submit_btn = gr.Button("Give command", variant="primary")
             submit_btn.click(fn=transcribe, inputs=audio_input, outputs=output, api_name="record")
     with gr.Accordion("Examples:"):
         gr.Markdown("Explore the second floor of the green house.\n\n"
