cc9b72d4178d84f572803624e2be5b55ecb404aa
Sydney Nguyen <melosydrose@gmail.com>
2023-07-06 00:03:17 -0500

    Integrate get_transcript and format_command chains

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-import os
-from typing import Optional, Tuple
-
 import openai
 import gradio as gr
-from threading import Lock
-
-from langchain import ConversationChain
-
-# from chatgpt4uavs.py import sentence_command_chain
+import chatgpt4uavs
 
 openai.api_key = "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"
 
@@ -21,36 +14,20 @@ messages = [
 ]
 
 
-def load_chain():
-    # need to load chains from chatgpt4uavs.py
-    pass
-
-
-def set_openai_api_key(api_key: str):
-    if api_key:
-        os.environ["OPENAI_API_KEY"] = api_key
-        chain = load_chain()
-        os.environ["OPENAI_API_KEY"] = ""
-        return chain
-
-
 def transcribe(audio):
     global messages
 
     audio_file = open(audio, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print("TRANSCRIPT:", transcript["text"])
 
-    messages.append({"role": "user", "content": transcript["text"]})
+    # Gradio runs well but having issues with output saying "none" even though audio was recorded
+    transcript_chain = chatgpt4uavs.get_transcription(transcript["text"])
+    print("TRANSCRIPT CHAIN:", transcript_chain)
+    formatted_command = chatgpt4uavs.format_command(transcript_chain)
+    print("FORMATTED COMMAND:", formatted_command)
 
-    """Goal: Integrate LangChain into Gradio - my approach is to grab the original transcript above,
-    run it through the chains from chatgpt4uavs.py and save it to filtered_transcript, and use the
-    filtered transcript in the conversation with the conversational UAV.
-    
-    Currently getting a few errors with the sentence_command_chain, specifically the issue lies
-    within PromptTemplate in chatgpt4uavs.py, so I will need to check that. I commented the code
-    out to test the interface without errors."""
-    # filtered_transcript = sentence_command_chain.run({"text": transcript["text"]})
-    # messages.append({"role": "user", "content": filtered_transcript["output"]})
+    messages.append({"role": "user", "content": formatted_command})
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -69,45 +46,11 @@ def transcribe(audio):
     return chat_transcript
 
 
-class ChatWrapper:
-    def __init__(self):
-        self.lock = Lock()
-
-    def __call__(
-            self, api_key: str, audio_input, state: Optional[Tuple[str, str]], chain: Optional[ConversationChain]
-    ):
-        self.lock.acquire()
-        try:
-            if chain is None:
-                state.append(("Please paste your OpenAI key to use",))
-                return state, state
-            openai.api_key = api_key
-
-            transcript = transcribe(audio_input)
-            state.append((transcript,))
-
-        except Exception as e:
-            raise e
-        finally:
-            self.lock.release()
-
-        return state, state
-
-
-chat = ChatWrapper()
-
 with gr.Blocks(theme='sudeepshouche/minimalist') as demo:
     gr.Markdown("""
     # Conversational UAV Explorer
     Speak into the microphone to give a command.\n\n
     Commands: Explore, Take Picture, Monitor, Stop, Land, Come Back, Left, Right, Up, Down, Get Closer""")
-    with gr.Row():
-        openai_api_key_textbox = gr.Textbox(
-            placeholder="Paste your OpenAI API key",
-            show_label=False,
-            lines=1,
-            type="password"
-        )
     with gr.Row().style():
         audio_input = gr.Audio(source="microphone", type="filepath")
         output = gr.Textbox(label="Transcript")
@@ -119,13 +62,5 @@ with gr.Blocks(theme='sudeepshouche/minimalist') as demo:
         gr.Markdown("Explore the second floor of the green house.\n\n"
                     "Take a picture inside the brick house on Main St.\n\n"
                     "Monitor the house at the NE corner of the intersection of Main and Cary.")
-
-    state = gr.State()
-
-    openai_api_key_textbox.change(
-        set_openai_api_key,
-        inputs=[openai_api_key_textbox],
-        outputs=[state],
-    )
 
 demo.launch(share=True)
