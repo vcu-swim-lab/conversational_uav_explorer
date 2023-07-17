0e493cfcfe22161507fa182e53af083b7907d408
sienna-sterling <112006749+sienna-sterling@users.noreply.github.com>
2023-07-16 22:13:50 -0400

    Update test_chatgpt4uavs.py
    More tests

--- test/test_chatgpt4uavs.py
+++ test/test_chatgpt4uavs.py
@@ -15,3 +15,87 @@ def test_red_house():
     output = get_command(text)
     command = parse_command(output)
     assert command == "go to"
+
+def test_red_house():
+    text = "Go to the red house on W Broad St."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "go to"
+
+def test_take_off():
+    text = "Take off from where you are."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "take off"
+
+def test_land():
+    text = "Land now."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "land"
+
+def test_picture():
+    text = "Take a picture."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "take picture"
+
+def test_snap_pic():
+    text = "Snap a photo."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "take picture"
+
+def test_purple_house():
+    text = "Travel to the purple house on the left side."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "go to"
+
+def test_parking_garage():
+    text = "Fly to the parking garage across the street."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "go to"
+
+def test_gym():
+    text = "Check out the Cary St Gym."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "go to"
+
+def test_cease_flight():
+    text = "Cease flight."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "land"
+
+def test_stop_flight():
+    text = "Stop the flight."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "land"
+
+def test_lift_off():
+    text = "Lift off."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "take off"
+
+def test_no_location1():
+    text = "Check out."
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "None"
+
+def test_no_location2():
+    text = "Go to"
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "None"
+
+def test_no_location3():
+    text = "Travel to"
+    output = get_command(text)
+    command = parse_command(output)
+    assert command == "None"
