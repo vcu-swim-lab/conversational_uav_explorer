ff028900724e617620e5a1878c6bec203c1b90bd
sienna-sterling <112006749+sienna-sterling@users.noreply.github.com>
2023-07-16 22:14:33 -0400

    Update test_fewshot.py
    Adding tests

--- test/test_fewshot.py
+++ test/test_fewshot.py
@@ -18,3 +18,81 @@ def test_red_house(fewshot):
     output = fewshot.get_command(text)
     command = parse_command(output)
     assert command == "go to"
+
+def test_take_off(fewshot):
+    text = "Take off from where you are."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "take off"
+
+def test_land(fewshot):
+    text = "Land now."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "land"
+
+def test_picture(fewshot):
+    text = "Take a picture."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "take picture"
+
+def test_snap_pic(fewshot):
+    text = "Snap a photo."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "take picture"
+
+def test_purple_house(fewshot):
+    text = "Travel to the purple house on the left side."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "go to"
+
+def test_parking_garage(fewshot):
+    text = "Fly to the parking garage across the street."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "go to"
+
+def test_gym(fewshot):
+    text = "Check out the Cary St Gym."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "go to"
+
+def test_cease_flight(fewshot):
+    text = "Cease flight."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "land"
+
+def test_stop_flight(fewshot):
+    text = "Stop the flight."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "land"
+
+def test_lift_off(fewshot):
+    text = "Lift off."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "take off"
+
+def test_no_location1(fewshot):
+    text = "Check out."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "None"
+
+def test_no_location2(fewshot):
+    text = "Go to"
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "None"
+
+def test_no_location3(fewshot):
+    text = "Travel to"
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "None"
