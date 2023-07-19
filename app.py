cbddf638df8c6b7f53d1258f4c0546416ae88775
Sydney Nguyen <melosydrose@gmail.com>
2023-07-19 13:48:21 -0400

    Update UI to reflect current version

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import openai
 import gradio as gr
 import chatgpt4uavs
+from maps import AddressLocator
 from prompts import prompt_chat_response
 from fewshot import FewShot4UAVs
 
 openai.api_key = "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"
 
 fewshot = FewShot4UAVs()
+address_locator = AddressLocator()
 
 def transcribe(audio):
     messages = [
@@ -19,6 +21,10 @@ def transcribe(audio):
 
     # formatted_command_text = chatgpt4uavs.get_command(transcript["text"])
     formatted_command_text = fewshot.get_command(transcript["text"])
+    location = address_locator.get_location(formatted_command_text)
+
+    if location is not None:
+        formatted_command_text = formatted_command_text.replace("<location>", location)
 
     messages.append({"role": "user", "content": formatted_command_text})
 
@@ -43,7 +49,7 @@ with gr.Blocks(theme='sudeepshouche/minimalist') as demo:
     gr.Markdown("""
     # Conversational UAV Explorer
     Speak into the microphone to give a command.\n\n
-    Commands: Explore, Take Picture, Monitor, Stop, Land, Come Back, Left, Right, Up, Down, Get Closer""")
+    Commands: Take picture, Go to, Land, Take off""")
     with gr.Row().style():
         audio_input = gr.Audio(source="microphone", type="filepath")
         output = gr.Textbox(label="Transcript")
@@ -52,8 +58,9 @@ with gr.Blocks(theme='sudeepshouche/minimalist') as demo:
             submit_btn = gr.Button("Give command", variant="primary")
             submit_btn.click(fn=transcribe, inputs=audio_input, outputs=output, api_name="record")
     with gr.Accordion("Examples:"):
-        gr.Markdown("Explore the second floor of the green house.\n\n"
-                    "Take a picture inside the brick house on Main St.\n\n"
-                    "Monitor the house at the NE corner of the intersection of Main and Cary.")
+        gr.Markdown("Take a picture of the second floor of the green house.\n\n"
+                    "Go to the brick house across the street.\n\n"
+                    "Land now.\n\n"
+                    "Take off.")
 
 demo.launch(share=True)
