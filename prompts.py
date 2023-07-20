8a621493a8077f37a02347ec8311bbb2273e129e
Sydney Nguyen <melosydrose@gmail.com>
2023-07-20 19:21:33 -0400

    Add more tests, prompts

--- prompts.py
+++ prompts.py
@@ -12,7 +12,7 @@ You are going to be given a sentence command, you need to find the action of
 the sentence. The action will be, Take Picture, Take Off, Land or Go To. 
 If the action is "Take Off" or "Land" you don't need any further information
 for the location. If the action is "Take Picture" or "Go To" you'll need to 
-find where to carry out the action. You need to find. If you can't find an 
+find where to carry out the action. If you can't find an 
 action or location, answer with "none". You need to return the command
 in this format: command <command> \t <goal>
 
@@ -31,11 +31,43 @@ needed, and provide updates on the execution status of the given commands.
 # Also passing the transcription to the Command Prompt Template.
 examples_few_shot = [
     {
-        "sentence": "Go to",
+        "sentence": "Go to Papa Johns at 1200 W Main St",
+        "command": "command: go to \tPapa Johns at 1200 W Main St"""
+    },
+    {
+        "sentence": "Go to the orange house on W Grace St",
+        "command": "command: go to \torange house on W Grace St"""
+    },
+    {
+        "sentence": "Travel to Cabell Library at VCU",
+        "command": "command: go to \tCabell Library at VCU"""
+    },
+    {
+        "sentence": "Kroger on N Lombardy St",
+        "command": "None"""
+    },
+    {
+        "sentence": "CVS at Main St",
+        "command": "None"""
+    },
+    {
+        "sentence": "Short Pump Town Center",
+        "command": "None"""
+    },
+    {
+        "sentence": "Can Can Brasserie",
+        "command": "None"""
+    },
+    {
+        "sentence": "Walgreens",
         "command": "None"""
     },
     {
         "sentence": "Travel to",
+        "command": "None"""
+    },
+    {
+        "sentence": "Go to",
         "command": "None"""
     },
     {
@@ -59,19 +91,11 @@ examples_few_shot = [
         "command": "command: take off"
     },
     {
-        "sentence": "Go up and go",
-        "command": "command: take off"
-    },
-    {
         "sentence": "Lift off.",
         "command": "command: take off"
     },
     {
         "sentence": "Land now.",
-        "command": "command: land"
-    },
-    {
-        "sentence": "Go to the ground where you are.",
         "command": "command: land"
     },
     {
@@ -83,72 +107,12 @@ examples_few_shot = [
         "command": "command: take picture"
     },
     {
-        "sentence": "Go to the 7/11 across the street.",
-        "command": "command: go to \t7/11 across the street"
-    },
-    {
-        "sentence": "Go to the yellow house two houses to the right of here.",
-        "command": "command: go to \tyellow house two houses to the right of here"""
-    },
-    {
-        "sentence": "Go to.",
-        "command": "None"
-    },
-    {
-        "sentence": "Go to the Walmart on Iron Bridge Road.",
-        "command": "command: go to \tWalmart on Iron Bridge Road"""
-    },
-    {
-        "sentence": "Go to the bank on the corner.",
-        "command": "command: go to \tbank on the corner"""
-    },
-    {
-        "sentence": "Travel to the bank.",
-        "command": "command: go to \tbank"""
-    },
-    {
-        "sentence": "Travel.",
-        "command": "None"""
-    },
-    {
         "sentence": "Stop flying",
         "command": "command: land"""
     },
     {
         "sentence": "Take flight",
         "command": "command: take off"""
-    },
-    {
-        "sentence": "Travel to the purple house on the left side.",
-        "command": "command: go to \tpurple house on the left side"""
-    },
-    {
-        "sentence": "Travel to the Barnes and Noble on West Broad.",
-        "command": "command: go to \tBarnes and Noble on West Broad"""
-    },
-    {
-        "sentence": "Travel to that place.",
-        "command": "None"""
-    },
-    {
-        "sentence": "Travel to there.",
-        "command": "None"""
-    },
-    {
-        "sentence": "Travel there.",
-        "command": "None"""
-    },
-    {
-        "sentence": "Travel over there.",
-        "command": "None"""
-    },
-    {
-        "sentence": "Travel to the bank on the corner.",
-        "command": "command: go to \tbank on the corner"""
-    },
-    {
-        "sentence": "Travel to the Walmart on W Broad St.",
-        "command": "command: go to \tWalmart on W Broad St"""
     },
     {
         "sentence": "Take a pic.",
@@ -167,36 +131,12 @@ examples_few_shot = [
         "command": "command: take picture"
     },
     {
-        "sentence": "Fly to the parking garage across the street.",
-        "command": "command: go to \tparking garage across the street"
-    },
-    {
         "sentence": "Fly to the Sonic on West Cary St.",
         "command": "command: go to \tSonic on West Cary St."
     },
     {
-        "sentence": "Check out the pizza shop next to the red house over there.",
-        "command": "command: go to \tpizza shop next to the red house over there."
-    },
-    {
-        "sentence": "Check out the Cary St Gym.",
-        "command": "command: go to \tthe Cary St Gym"
-    },
-    {
-        "sentence": "Check out",
-        "command": "None"
-    },
-    {
-        "sentence": "Travel to",
-        "command": "None"
-    },
-    {
-        "sentence": "Get to",
-        "command": "None"
-    },
-    {
-        "sentence": "Navigate to",
-        "command": "None"
+        "sentence": "Check out the gym at Cary St.",
+        "command": "command: go to \tgym at Cary St"
     },
     {
         "sentence": "Cease flight",
@@ -207,11 +147,11 @@ examples_few_shot = [
         "command": "command: land"
     },
     {
-        "sentence": "Investigate the green house to the left.",
-        "command": "command: go to \tgreen house to the left"
-    },
-    {
         "sentence": "Investigate the Rite Aid on Broad and Belevidere.",
         "command": "command: go to \tRite Aid on Broad and Belevidere"
+    },
+    {
+        "sentence": "Please check out the CVS on W Broad St.",
+        "command": "command: go to \tCVS on W Broad St"
     }
 ]
--- test/test_fewshot.py
+++ test/test_fewshot.py
@@ -2,9 +2,11 @@ import pytest
 import re
 from fewshot import FewShot4UAVs
 
+
 @pytest.fixture
 def fewshot():
     return FewShot4UAVs()
+
 
 # format: command: <command> \t<goal>
 def parse_command(text):
@@ -15,11 +17,13 @@ def parse_command(text):
     else:
         return 'None'
 
+
 def test_red_house(fewshot):
     text = "Go to the red house on W Broad St."
     output = fewshot.get_command(text)
     command = parse_command(output)
     assert command == "go to"
+
 
 def test_take_off(fewshot):
     text = "Take off from where you are."
@@ -27,11 +31,20 @@ def test_take_off(fewshot):
     command = parse_command(output)
     assert command == "take off"
 
+
+def test_take_off_synonym(fewshot):
+    text = "Lift off."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "take off"
+
+
 def test_land(fewshot):
     text = "Land now."
     output = fewshot.get_command(text)
     command = parse_command(output)
     assert command == "land"
+
 
 def test_picture(fewshot):
     text = "Take a picture."
@@ -39,11 +52,13 @@ def test_picture(fewshot):
     command = parse_command(output)
     assert command == "take picture"
 
+
 def test_snap_pic(fewshot):
     text = "Snap a photo."
     output = fewshot.get_command(text)
     command = parse_command(output)
     assert command == "take picture"
+
 
 def test_purple_house(fewshot):
     text = "Travel to the purple house on the left side."
@@ -51,11 +66,13 @@ def test_purple_house(fewshot):
     command = parse_command(output)
     assert command == "go to"
 
+
 def test_parking_garage(fewshot):
     text = "Fly to the parking garage across the street."
     output = fewshot.get_command(text)
     command = parse_command(output)
     assert command == "go to"
+
 
 def test_gym(fewshot):
     text = "Check out the Cary St Gym."
@@ -63,11 +80,13 @@ def test_gym(fewshot):
     command = parse_command(output)
     assert command == "go to"
 
+
 def test_cease_flight(fewshot):
     text = "Cease flight."
     output = fewshot.get_command(text)
     command = parse_command(output)
     assert command == "land"
+
 
 def test_stop_flight(fewshot):
     text = "Stop the flight."
@@ -75,11 +94,6 @@ def test_stop_flight(fewshot):
     command = parse_command(output)
     assert command == "land"
 
-def test_lift_off(fewshot):
-    text = "Lift off."
-    output = fewshot.get_command(text)
-    command = parse_command(output)
-    assert command == "take off"
 
 def test_no_location1(fewshot):
     text = "Check out."
@@ -87,14 +101,114 @@ def test_no_location1(fewshot):
     command = parse_command(output)
     assert command == "None"
 
+
 def test_no_location2(fewshot):
     text = "Go to"
     output = fewshot.get_command(text)
     command = parse_command(output)
     assert command == "None"
 
+
 def test_no_location3(fewshot):
     text = "Travel to"
     output = fewshot.get_command(text)
     command = parse_command(output)
     assert command == "None"
+
+
+def test_no_location4(fewshot):
+    text = "Head to"
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "None"
+
+
+def test_empty_input(fewshot):
+    text = ""
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "None"
+
+
+def test_invalid_command(fewshot):
+    text = "Do something"
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "None"
+
+
+def test_location_no_action(fewshot):
+    text = "Willow Lawn"
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "None"
+
+
+def test_location_no_action2(fewshot):
+    text = "Whole Foods on W Broad St"
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "None"
+
+
+def test_location_no_action3(fewshot):
+    text = "Starbucks at N Robinson St"
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "None"
+
+
+def test_with_extra_words(fewshot):
+    text = "Please go to the Whole Foods on W Broad St"
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "go to"
+
+def test_with_extra_words2(fewshot):
+    text = "Go to the Whole Foods on W Broad St please"
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == "go to"
+
+
+
+"""
+
+def test_multiple_actions(fewshot):
+    text = "Take a photo and then take off."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == 
+
+
+def test_two_go_to(fewshot):
+    text = "Go to the Kroger on Lombardy St and then go to Roots on W Grace St."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == 
+
+
+def test_multiple_actions_and_locations(fewshot):
+    text = "Fly to the park and take a picture of the red house."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == 
+
+def test_action_and_location(fewshot):
+    text = "Fly to the park and take a picture."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == 
+
+def test_action_and_location_synonyms(fewshot):
+    text = "Hover and head to the closest park."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == 
+
+def test_action_synonyms(fewshot):
+    text = "Ascend and then take a photo."
+    output = fewshot.get_command(text)
+    command = parse_command(output)
+    assert command == 
+"""
