2c52b7be10f32810c5c9e9601b637ef5b03561ab
Sydney Nguyen <melosydrose@gmail.com>
2023-07-19 20:19:27 -0400

    Add more examples for few shot

--- prompts.py
+++ prompts.py
@@ -31,123 +31,187 @@ needed, and provide updates on the execution status of the given commands.
 # Also passing the transcription to the Command Prompt Template.
 examples_few_shot = [
     {
-    "sentence": "Take off now.",
-    "command": "command: take off"""
+        "sentence": "Go to",
+        "command": "None"""
     },
     {
-    "sentence": "Take off from where you are.",
-    "command": "command: take off"
+        "sentence": "Travel to",
+        "command": "None"""
     },
     {
-    "sentence": "Go up and go",
-    "command": "command: take off"
+        "sentence": "Take off now.",
+        "command": "command: take off"""
     },
     {
-    "sentence": "Lift off.",
-    "command": "command: take off"
+        "sentence": "Take off from where you are.",
+        "command": "command: take off"
     },
     {
-    "sentence": "Land now.",
-    "command": "command: land"
+        "sentence": "Take off.",
+        "command": "command: take off"
     },
     {
-    "sentence": "Go to the ground where you are.",
-    "command": "command: land"
+        "sentence": "Take off at your position.",
+        "command": "command: take off"
     },
     {
-    "sentence": "Take a picture.",
-    "command": "command: take picture"
+        "sentence": "Take off from where you are at.",
+        "command": "command: take off"
     },
     {
-    "sentence": "Take a picture when you can please.",
-    "command": "command: take picture"    
+        "sentence": "Go up and go",
+        "command": "command: take off"
     },
     {
-    "sentence": "Go to the 7/11 across the street.",
-    "command": "command: go to \t7/11 across the street"
+        "sentence": "Lift off.",
+        "command": "command: take off"
     },
     {
-    "sentence": "Go to the yellow house two houses to the right of here.",
-    "command": "command: go to \tyellow house two houses to the right of here"""
+        "sentence": "Land now.",
+        "command": "command: land"
     },
     {
-    "sentence": "Go to.",
-    "command": "None"
+        "sentence": "Go to the ground where you are.",
+        "command": "command: land"
     },
     {
-    "sentence": "Go to the Walmart on Iron Bridge Road.",
-    "command": "command: go to \tWalmart on Iron Bridge Road"""
+        "sentence": "Take a picture.",
+        "command": "command: take picture"
     },
     {
-    "sentence": "Stop flying",
-    "command": "command: land"""
+        "sentence": "Take a picture when you can please.",
+        "command": "command: take picture"
     },
     {
-    "sentence": "Take flight",
-    "command": "command: take off"""
+        "sentence": "Go to the 7/11 across the street.",
+        "command": "command: go to \t7/11 across the street"
     },
     {
-    "sentence": "Travel to the purple house on the left side.",
-    "command": "command: go to \tpurple house on the left side"""
+        "sentence": "Go to the yellow house two houses to the right of here.",
+        "command": "command: go to \tyellow house two houses to the right of here"""
     },
     {
-    "sentence": "Travel to the Barnes and Noble on West Broad.",
-    "command": "command: go to \tBarnes and Noble on West Broad"""
+        "sentence": "Go to.",
+        "command": "None"
     },
     {
-    "sentence": "Take a pic.",
-    "command": "command: take picture"    
+        "sentence": "Go to the Walmart on Iron Bridge Road.",
+        "command": "command: go to \tWalmart on Iron Bridge Road"""
     },
     {
-    "sentence": "Take a photo.",
-    "command": "command: take picture"    
+        "sentence": "Go to the bank on the corner.",
+        "command": "command: go to \tbank on the corner"""
     },
     {
-    "sentence": "Snap a photo.",
-    "command": "command: take picture"    
+        "sentence": "Travel to the bank.",
+        "command": "command: go to \tbank"""
     },
     {
-    "sentence": "Snap a pic.",
-    "command": "command: take picture"    
+        "sentence": "Travel.",
+        "command": "None"""
     },
     {
-    "sentence": "Fly to the parking garage across the street.",
-    "command": "command: go to \tparking garage across the street"
+        "sentence": "Stop flying",
+        "command": "command: land"""
     },
     {
-    "sentence": "Fly to the Sonic on West Cary St.",
-    "command": "command: go to \tSonic on West Cary St."
+        "sentence": "Take flight",
+        "command": "command: take off"""
     },
     {
-    "sentence": "Check out the pizza shop next to the red house over there.",
-    "command": "command: go to \tpizza shop next to the red house over there."
+        "sentence": "Travel to the purple house on the left side.",
+        "command": "command: go to \tpurple house on the left side"""
     },
     {
-    "sentence": "Check out the Cary St Gym.",
-    "command": "command: go to \tthe Cary St Gym"
+        "sentence": "Travel to the Barnes and Noble on West Broad.",
+        "command": "command: go to \tBarnes and Noble on West Broad"""
     },
     {
-    "sentence": "Check out",
-    "command": "None"
+        "sentence": "Travel to that place.",
+        "command": "None"""
     },
     {
-    "sentence": "Travel to",
-    "command": "None"
+        "sentence": "Travel to there.",
+        "command": "None"""
     },
     {
-    "sentence": "Cease flight",
-    "command": "land"
+        "sentence": "Travel there.",
+        "command": "None"""
     },
     {
-    "sentence": "Stop the flight",
-    "command": "land"
+        "sentence": "Travel over there.",
+        "command": "None"""
     },
     {
-    "sentence": "Investigate the green house to the left.",
-    "command": "command: go to \tgreen house to the left"
+        "sentence": "Travel to the bank on the corner.",
+        "command": "command: go to \tbank on the corner"""
     },
     {
-    "sentence": "Investigate the Rite Aid on Broad and Belevidere.",
-    "command": "command: go to \tRite Aid on Broad and Belevidere"
+        "sentence": "Travel to the Walmart on W Broad St.",
+        "command": "command: go to \tWalmart on W Broad St"""
+    },
+    {
+        "sentence": "Take a pic.",
+        "command": "command: take picture"
+    },
+    {
+        "sentence": "Take a photo.",
+        "command": "command: take picture"
+    },
+    {
+        "sentence": "Snap a photo.",
+        "command": "command: take picture"
+    },
+    {
+        "sentence": "Snap a pic.",
+        "command": "command: take picture"
+    },
+    {
+        "sentence": "Fly to the parking garage across the street.",
+        "command": "command: go to \tparking garage across the street"
+    },
+    {
+        "sentence": "Fly to the Sonic on West Cary St.",
+        "command": "command: go to \tSonic on West Cary St."
+    },
+    {
+        "sentence": "Check out the pizza shop next to the red house over there.",
+        "command": "command: go to \tpizza shop next to the red house over there."
+    },
+    {
+        "sentence": "Check out the Cary St Gym.",
+        "command": "command: go to \tthe Cary St Gym"
+    },
+    {
+        "sentence": "Check out",
+        "command": "None"
+    },
+    {
+        "sentence": "Travel to",
+        "command": "None"
+    },
+    {
+        "sentence": "Get to",
+        "command": "None"
+    },
+    {
+        "sentence": "Navigate to",
+        "command": "None"
+    },
+    {
+        "sentence": "Cease flight",
+        "command": "command: land"
+    },
+    {
+        "sentence": "Stop the flight",
+        "command": "command: land"
+    },
+    {
+        "sentence": "Investigate the green house to the left.",
+        "command": "command: go to \tgreen house to the left"
+    },
+    {
+        "sentence": "Investigate the Rite Aid on Broad and Belevidere.",
+        "command": "command: go to \tRite Aid on Broad and Belevidere"
     }
 ]
