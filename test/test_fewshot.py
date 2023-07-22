a0a38d2b73c1a5bb572e11d50c03a92f04736249
Sydney Nguyen <melosydrose@gmail.com>
2023-07-21 22:25:03 -0400

    Update tests to expect command as one word

--- test/test_fewshot.py
+++ test/test_fewshot.py
@@ -8,7 +8,7 @@ def fewshot():
     return FewShot4UAVs()
 
 
-# format: command: <command> \t<goal>
+# format: <command> \t<goal>
 def parse_command(text):
     tokens = re.split(' \t|\n|: ', text.lower())
     print(tokens)
@@ -22,21 +22,21 @@ def test_red_house(fewshot):
     text = "Go to the red house on W Broad St."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "go to"
+    assert command == "goto"
 
 
 def test_take_off(fewshot):
     text = "Take off from where you are."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "take off"
+    assert command == "takeoff"
 
 
 def test_take_off_synonym(fewshot):
     text = "Lift off."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "take off"
+    assert command == "takeoff"
 
 
 def test_land(fewshot):
@@ -64,21 +64,21 @@ def test_purple_house(fewshot):
     text = "Travel to the purple house on the left side."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "go to"
+    assert command == "goto"
 
 
 def test_parking_garage(fewshot):
     text = "Fly to the parking garage across the street."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "go to"
+    assert command == "goto"
 
 
 def test_gym(fewshot):
     text = "Check out the Cary St Gym."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "go to"
+    assert command == "goto"
 
 
 def test_cease_flight(fewshot):
@@ -141,34 +141,34 @@ def test_location_no_action(fewshot):
     text = "Willow Lawn."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "go to"
+    assert command == "goto"
 
 
 def test_location_no_action2(fewshot):
     text = "Whole Foods on W Broad St."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "go to"
+    assert command == "goto"
 
 
 def test_location_no_action3(fewshot):
     text = "Starbucks at N Robinson St."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "go to"
+    assert command == "goto"
 
 
 def test_with_extra_words(fewshot):
     text = "Please go to the Whole Foods on W Broad St."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "go to"
+    assert command == "goto"
 
 def test_with_extra_words2(fewshot):
     text = "Go to the Whole Foods on W Broad St please."
     output = fewshot.get_command(text)
     command = parse_command(output)
-    assert command == "go to"
+    assert command == "goto"
 
 
 
