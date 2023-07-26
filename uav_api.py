fa6d1d67420e3f0332465d440eb0712713515a81
Sydney Nguyen <melosydrose@gmail.com>
2023-07-26 16:22:01 -0400

    Create HTTP POST request method

--- uav_api.py
+++ uav_api.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+import requests
 from gradio_client import Client
-
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,13 +22,34 @@ def get_latest_command():
     return command
 
 
+def send_command_to_uav_controller(command):
+    url = "http://localhost:8080"  # replace with address for UAV's C++ code later
+    data = {"command": command}
+    response = requests.post(url, json=data)
+    return response.text
+
+
 class Command(Resource):
     def get(self):
         latest_command = get_latest_command()
         return jsonify({"latest_command": latest_command})
 
+    def post(self):
+        latest_command = get_latest_command()
+        if latest_command:
+            response = send_command_to_uav_controller(latest_command)
+            return jsonify({"status": "Command sent to UAV controller.", "response": response}), 200
+        else:
+            return jsonify({"status": "No command available."}), 404
 
-api.add_resource(Command, '/record')
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+api.add_resource(Command, '/command')
 
 if __name__ == '__main__':
     app.run(debug=True)
